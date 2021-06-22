87280a906a2110c06c797b9b251c5d09588cc04d
rahul07sharmars <58891614+rahul07sharmars@users.noreply.github.com>
2021-06-22 17:17:02 +0530

    commit

--- modular_arthimetic.cpp
+++ modular_arthimetic.cpp
@@ -4,24 +4,22 @@
 #include <bits/stdc++.h>
 #define mod 1e9 + 7;
 using namespace std;
-int mod_add(int a, int b, int m)
-{
-    a = a % m;
-    b = b % m;
-    return (((a + b) % m) + m) % m;
-}
-int mod_mul(int a, int b, int m)
-{
-    a = a % m;
-    b = b % m;
-    return (((a * b) % m) + m) % m;
-}
-int mod_sub(int a, int b, int m)
-{
-    a = a % m;
-    b = b % m;
-    return (((a - b) % m) + m) % m;
-}
+
+int expo(int a, int b, int mod) {int res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+
+int mminvprime(int a, int b) {return expo(a, b - 2, b);}
+// addition operation
+int mod_add(int a, int b, int m) {
+    a = a % m; b = b % m; return (((a + b) % m) + m) % m; }
+//multiplication
+int mod_mul(int a, int b, int m) {
+    a = a % m; b = b % m; return (((a * b) % m) + m) % m; }
+// subtration
+int mod_sub(int a, int b, int m){
+    a = a % m; b = b % m; return (((a - b) % m) + m) % m; }
+// division
+int mod_div(int a, int b, int m) {
+    a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
 signed main(){
 
     cout<<"hello world\n";
